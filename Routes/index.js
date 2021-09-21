9a9e66106f83c3d3c5221c2f2efe0c3b14e1de70
RayendraSabandar <rayendratimotius@gmail.com>
2021-09-21 18:49:36 +0700

    Deleted username in database

--- Routes/index.js
+++ Routes/index.js
@@ -2,12 +2,16 @@ const router = require('express').Router()
 const UserRoutes = require('./user')
 const CocktailController = require('./cocktail')
 const TagController = require('./tag')
+const { authentication } = require('../middlewares/authentication')
 
 router.get('/', (req, res) => {
     res.send(`handling "/"`)
 })
 
 router.use('/users', UserRoutes)
+
+router.use(authentication)
+
 router.use('/cocktails', CocktailController)
 router.use('/tags', TagController)
 
